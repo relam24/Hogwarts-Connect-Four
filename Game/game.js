400efb635232c0924fe816c24de7096a95edd279
Rachel Elam <relam24@gmail.com>
2018-03-02 23:24:25 -0500

    added ID's to each circle

--- Game/game.js
+++ Game/game.js
@@ -34,6 +34,22 @@
 
 
 
+// grab container div
+const $container = $('.container');
+// grab gameboard div
+const $gameboard = $('.gameboard');
+// grab column1 div
+const $column1 = $('.column1');
+// grab column2 div
+const $column2 = $('.column2');
+// grab column3 div
+const $column3 = $('.column3');
+// grab column4 div
+const $column4 = $('.column4');
+// grab column5 div
+const $column5 = $('.column5');
+// grab column 6 div
+const $column6 = $('.column6');
 
 
 
