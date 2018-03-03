f468defebb119511235e7b609298572862b71637
Rachel Elam <relam24@gmail.com>
2018-03-03 11:15:15 -0500

    added circles to JS

--- Game/game.js
+++ Game/game.js
@@ -50,24 +50,49 @@ $(() => {
 
 
 
-// create 'filled' circles
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// create circles
+// column 1
+	const $circle1A = $('#circle1A');
+	const $circle1B = $('#circle1B');
+	const $circle1C = $('#circle1C');
+	const $circle1D = $('#circle1D');
+	const $circle1E = $('#circle1E');
+	const $circle1F = $('#circle1F');
+// column 2
+	const $circle2A = $('#circle2A');
+	const $circle2B = $('#circle2B');
+	const $circle2C = $('#circle2C');
+	const $circle2D = $('#circle2D');
+	const $circle2E = $('#circle2E');
+	const $circle2F = $('#circle2F');
+// column 3
+	const $circle3A = $('#circle3A');
+	const $circle3B = $('#circle3B');
+	const $circle3C = $('#circle3C');
+	const $circle3D = $('#circle3D');
+	const $circle3E = $('#circle3E');
+	const $circle3F = $('#circle3F');
+ // column 4
+	const $circle4A = $('#circle4A');
+	const $circle4B = $('#circle4A');
+	const $circle4C = $('#circle4A');
+	const $circle4D = $('#circle4A');
+	const $circle4E = $('#circle4A');
+	const $circle4F = $('#circle4A');
+// column 5
+	const $circle5A = $('#circle5A');
+	const $circle5B = $('#circle5B');
+	const $circle5C = $('#circle5C');
+	const $circle5D = $('#circle5D');
+	const $circle5E = $('#circle5E');
+	const $circle5F = $('#circle5F');
+// column 6
+	const $circle6A = $('#circle6A');
+	const $circle6B = $('#circle6B');
+	const $circle6C = $('#circle6C');
+	const $circle6D = $('#circle6D');
+	const $circle6E = $('#circle6E');
+	const $circle6F = $('#circle6F');
 // =========================================//
 // grab container div
 	const $container = $('.container');
