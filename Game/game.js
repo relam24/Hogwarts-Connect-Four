baa9a3bd1ac5667e715f7bd835fa4daf3cd225fb
Rachel Elam <relam24@gmail.com>
2018-03-04 15:20:27 -0500

    cleaned it up a bit still trying to figure out how to only get one circle to change

--- Game/game.js
+++ Game/game.js
@@ -32,23 +32,22 @@ $(() => {
 		console.log('column1');
 		click1++;
 		if (click1 === 1) {
-			$(event.currentTarget).append($circle1F) .addClass('clickedCircle');
+			$(event.currentTarget).append($circle1F);
 			gameClicks();
-			console.log('gameclicks');
 		} else if (click1 === 2) {
-			$(event.currentTarget).append($circle1E) .addClass('clickedCircle');
+			$(event.currentTarget).append($circle1E);
 			gameClicks();
 		} else if (click1 === 3) {
-			$(event.currentTarget).append($circle1D) .addClass('clickedCircle');
+			$(event.currentTarget).append($circle1D);
 			gameClicks();
 		} else if (click1 === 4) {
-			$(event.currentTarget).append($circle1C) .addClass('clickedCircle');
+			$(event.currentTarget).append($circle1C);
 			gameClicks();
 		} else if (click1 === 5) {
-			$(event.currentTarget).append($circle1B) .addClass('clickedCircle');
+			$(event.currentTarget).append($circle1B);
 			gameClicks();
 		} else if (click1 === 6) {
-			$(event.currentTarget).append($circle1A) .addClass('clickedCircle');
+			$(event.currentTarget).append($circle1A);
 			gameClicks();
 		}
 	};
@@ -59,22 +58,22 @@ $(() => {
 		console.log('column2');
 		click2++;
 		if (click2 === 1) {
-			$(event.currentTarget).append($circle2F) .addClass('clickedCircle');
+			$(event.currentTarget).append($circle2F);
 			gameClicks();
 		} else if (click2 === 2) {
-			$(event.currentTarget).append($circle2E) .addClass('clickedCircle');
+			$(event.currentTarget).append($circle2E);
 			gameClicks();
 		} else if (click2 === 3) {
-			$(event.currentTarget).append($circle2D) .addClass('clickedCircle');
+			$(event.currentTarget).append($circle2D);
 			gameClicks();
 		} else if (click2 === 4) {
-			$(event.currentTarget).append($circle2C) .addClass('clickedCircle');
+			$(event.currentTarget).append($circle2C);
 			gameClicks();
 		} else if (click2 === 5) {
-			$(event.currentTarget).append($circle2B) .addClass('clickedCircle');
+			$(event.currentTarget).append($circle2B);
 			gameClicks();
 		} else if (click2 === 6) {
-			$(event.currentTarget).append($circle2A) .addClass('clickedCircle');
+			$(event.currentTarget).append($circle2A);
 			gameClicks();
 		}
 	};
@@ -85,22 +84,22 @@ $(() => {
 		console.log('column3');
 		click3++;
 		if (click3 === 1) {
-			$(event.currentTarget).append($circle3F) .addClass('clickedCircle');
+			$(event.currentTarget).append($circle3F);
 			gameClicks();
 		} else if (click3 === 2) {
-			$(event.currentTarget).append($circle3E) .addClass('clickedCircle');
+			$(event.currentTarget).append($circle3E);
 			gameClicks();
 		} else if (click3 === 3) {
-			$(event.currentTarget).append($circle3D) .addClass('clickedCircle');
+			$(event.currentTarget).append($circle3D);
 			gameClicks();
 		} else if (click3 === 4) {
-			$(event.currentTarget).append($circle3C) .addClass('clickedCircle');
+			$(event.currentTarget).append($circle3C);
 			gameClicks();
 		} else if (click3 === 5) {
-			$(event.currentTarget).append($circle3B) .addClass('clickedCircle');
+			$(event.currentTarget).append($circle3B);
 			gameClicks();
 		} else if (click3 === 6) {
-			$(event.currentTarget).append($circle3A) .addClass('clickedCircle');
+			$(event.currentTarget).append($circle3A);
 			gameClicks();
 		}
 	};
@@ -111,22 +110,22 @@ $(() => {
 		console.log('column4');
 		click4++;
 		if (click4 === 1) {
-			$(event.currentTarget).append($circle4F) .addClass('clickedCircle');
+			$(event.currentTarget).append($circle4F);
 			gameClicks();
 		} else if (click4 === 2) {
-			$(event.currentTarget).append($circle4E) .addClass('clickedCircle');
+			$(event.currentTarget).append($circle4E);
 			gameClicks();
 		} else if (click4 === 3) {
-			$(event.currentTarget).append($circle4D) .addClass('clickedCircle');
+			$(event.currentTarget).append($circle4D);
 			gameClicks();
 		} else if (click4 === 4) {
-			$(event.currentTarget).append($circle4C) .addClass('clickedCircle');
+			$(event.currentTarget).append($circle4C);
 			gameClicks();
 		} else if (click4 === 5) {
-			$(event.currentTarget).append($circle4B) .addClass('clickedCircle');
+			$(event.currentTarget).append($circle4B);
 			gameClicks();
 		} else if (click4 === 6) {
-			$(event.currentTarget).append($circle4A) .addClass('clickedCircle');
+			$(event.currentTarget).append($circle4A);
 			gameClicks();
 		}
 	};
@@ -137,22 +136,22 @@ $(() => {
 		console.log('column5');
 		click5++;
 		if (click5 === 1) {
-			$(event.currentTarget).append($circle5F) .addClass('clickedCircle');
+			$(event.currentTarget).append($circle5F);
 			gameClicks();
 		} else if (click5 === 2) {
-			$(event.currentTarget).append($circle5E) .addClass('clickedCircle');
+			$(event.currentTarget).append($circle5E);
 			gameClicks();
 		} else if (click5 === 3) {
-			$(event.currentTarget).append($circle5D) .addClass('clickedCircle');
+			$(event.currentTarget).append($circle5D);
 			gameClicks();
 		} else if (click5 === 4) {
-			$(event.currentTarget).append($circle5C) .addClass('clickedCircle');
+			$(event.currentTarget).append($circle5C);
 			gameClicks();
 		} else if (click5 === 5) {
-			$(event.currentTarget).append($circle5B) .addClass('clickedCircle');
+			$(event.currentTarget).append($circle5B);
 			gameClicks();
 		} else if (click5 === 6) {
-			$(event.currentTarget).append($circle5A) .addClass('clickedCircle');
+			$(event.currentTarget).append($circle5A);
 			gameClicks();
 		}
 	};
@@ -163,24 +162,22 @@ $(() => {
 		console.log('column6');
 		click6++;
 		if (click6 === 1) {
-			$(event.currentTarget).append($circle6F) .addClass('clickedCircle');
+			$(event.currentTarget).append($circle6F);
 			gameClicks();
 		} else if (click6 === 2) {
-			$(event.currentTarget).append($circle6E) .addClass('clickedCircle');
+			$(event.currentTarget).append($circle6E);
 			gameClicks();
 		} else if (click6 === 3) {
-			$(event.currentTarget).append($circle6D) .addClass('clickedCircle');
+			$(event.currentTarget).append($circle6D);
 			gameClicks();
 		} else if (click6 === 4) {
-			$(event.currentTarget).append($circle6C) .addClass('clickedCircle');
+			$(event.currentTarget).append($circle6C);
 			gameClicks();
 		} else if (click6 === 5) {
-			$(event.currentTarget).append($circle6B)
-			.addClass('clickedCircle');
+			$(event.currentTarget).append($circle6B);
 			gameClicks();
 		} else if (click6 === 6) {
-			$(event.currentTarget).append($circle6A)
-			.addClass('clickedCircle');
+			$(event.currentTarget).append($circle6A);
 			gameClicks();
 		}
 	};
@@ -191,135 +188,134 @@ $(() => {
 		console.log('game clicks');
 		game++;
 		if (game === 1) {
-				playerOne();
+			playerOne();
 		} else if (game === 2) {
-				playerTwo();
+			playerTwo();
 		} else if (game === 3) {
-				playerOne();
+			playerOne();
 		} else if (game === 4) {
-				playerTwo();
-				youWin();
+			playerTwo();
+			youWin();
 		} else if (game === 5) {
-				playerOne();
-				youWin();
+			playerOne();
+			youWin();
 		} else if (game === 6) {
-				playerTwo();
-				youWin();
+			playerTwo();
+			youWin();
 		} else if (game === 7) {
-				playerOne();
-				youWin();
+			playerOne();
+			youWin();
 		} else if (game === 8) {
-				playerTwo();
-				youWin();
+			playerTwo();
+			youWin();
 		} else if (game === 9) {
-				playerOne();
-				youWin();
+			playerOne();
+			youWin();
 		} else if (game === 10) {
-				playerTwo();
-				youWin();
+			playerTwo();
+			youWin();
 		} else if (game === 11) {
-				playerOne();
-				youWin();
+			playerOne();
+			youWin();
 		} else if (game === 12) {
-				playerTwo();
-				youWin();
+			playerTwo();
+			youWin();
 		} else if (game === 13) {
-				playerOne();
-				youWin();
+			playerOne();
+			youWin();
 		} else if (game === 14) {
-				playerTwo();
-				youWin();
+			playerTwo();
+			youWin();
 		} else if (game === 15) {
-				playerOne();
-				youWin();
+			playerOne();
+			youWin();
 		} else if (game === 16) {
-				playerTwo();
-				youWin();
+			playerTwo();
+			youWin();
 		} else if (game === 17) {
-				playerOne();
-				youWin();
+			playerOne();
+			youWin();
 		} else if (game === 18) {
-				playerTwo();
-				youWin();
+			playerTwo();
+			youWin();
 		} else if (game === 19) {
-				playerOne();
-				youWin();
+			playerOne();
+			youWin();
 		} else if (game === 20) {
-				playerTwo();
-				youWin();
+			playerTwo();
+			youWin();
 		} else if (game === 21) {
-				playerOne();
-				youWin();
+			playerOne();
+			youWin();
 		} else if (game === 22) {
-				playerTwo();
-				youWin();
+			playerTwo();
+			youWin();
 		} else if (game === 23) {
-				playerOne();
-				youWin();
+			playerOne();
+			youWin();
 		} else if (game === 24) {
-				playerTwo();
-				youWin();
+			playerTwo();
+			youWin();
 		} else if (game === 25) {
-				playerOne();
-				youWin();
+			playerOne();
+			youWin();
 		} else if (game === 26) {
-				playerTwo();
-				youWin();
+			playerTwo();
+			youWin();
 		} else if (game === 27) {
-				playerOne();
-				youWin();
+			playerOne();
+			youWin();
 		} else if (game === 28) {
-				playerTwo()
-				youWin();
-				restart()
+			playerTwo()
+			youWin();
+			restart()
 		} else if (game === 29) {
-				playerOne();
-				youWin();
-				restart();
+			playerOne();
+			youWin();
+			restart();
 		} else if (game === 30) {
-				playerTwo();
-				youWin();
-				restart();
+			playerTwo();
+			youWin();
+			restart();
 		} else if (game === 31) {
-				playerOne();
-				youWin();
-				restart();
+			playerOne();
+			youWin();
+			restart();
 		} else if (game === 31) {
-				playerTwo();
-				youWin();
-				restart();
+			playerTwo();
+			youWin();
+			restart();
 		} else if (game === 32) {
-				playerOne();
-				youWin();
-				restart();
+			playerOne();
+			youWin();
+			restart();
 		} else if (game === 33) {
-				playerTwo();
-				youWin();
-				restart();
+			playerTwo();
+			youWin();
+			restart();
 		} else if (game === 34) {
-				playerOne();
-				youWin();
-				restart();
+			playerOne();
+			youWin();
+			restart();
 		} else if (game === 35) {
-				playerTwo();
-				youWin();
-				restart();
+			playerTwo();
+			youWin();
+			restart();
 		} else if (game === 36) {
-				playerOne();
-				youWin();
-				restart();
+			playerOne();
+			youWin();
+			restart();
 		}
 	};
 // ============================================//
 // playerOne
 	const playerOne = () => {
 		console.log('playerone');
-		$('.clickedCircle').css('background-color', 'red');
+
 	};
 // playerTwo
 	const playerTwo = () => {
 		console.log('playerTwo');
-		$('clicked').css('background-color', 'gold');
 	};
 // youWin
 	const youWin = () => {
@@ -381,6 +377,7 @@ $(() => {
 // grab column1 div
 // create click event
 // send to function
+// appended .clickedCircle to each column
 	const $column1 = $('.column1');
 	$column1.on('click', columnOneClick);
 // grab column2 div
