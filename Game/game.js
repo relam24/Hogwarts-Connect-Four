03f5b55037181f84ea618406af36f6400257c1d7
Rachel Elam <relam24@gmail.com>
2018-03-04 21:19:28 -0500

    array and for loops to get all of the values

--- Game/game.js
+++ Game/game.js
@@ -61,9 +61,12 @@ $(() => {
 			[$column1, $rowE], [$column2, $rowE], [$column3, $rowE], [$column4, $rowE], [$column5, $rowE], [$column6, $rowE],
 			[$column1, $rowF], [$column2, $rowF], [$column3, $rowF], [$column4, $rowF], [$column5, $rowF], [$column6, $rowF]
 		];
-		// for (let i = 0; i <= arrayRowColumn.length; i++) {
-		// 	console.log(arrayRowColumn[i]);
-		// }
+		for (let i = 0; i <= arrayRowColumn.length; i++) {
+			console.log(arrayRowColumn[i]);
+			for (let j = 0; j <= arrayRowColumn[i].length; j++) {
+				console.log(arrayRowColumn[j]);
+			}
+		}
 	};
 // play again/restart
 	const restart = () => {
